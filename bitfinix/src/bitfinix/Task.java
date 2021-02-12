4221483c31153697dded5e21998f3f12ec617f18
joe <joe@kali>
2021-02-12 22:01:59 +0300

    Added the TestNG Library and first class

---
+++ bitfinix/src/bitfinix/Task.java
@@ -0,0 +1,24 @@
+package bitfinix;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Task {
+  
+	@BeforeTest
+	public void Setup()
+	{
+		
+	}
+	@Test
+  public void loadPage()
+	{
+		
+  }
+	@AfterTest
+	public void stop()
+	{
+		
+	}
+}
