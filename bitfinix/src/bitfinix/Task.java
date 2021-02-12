0e6c0c1de99b4671e0d3ffe4a0f1a605af7dd116
joe <joe@kali>
2021-02-12 22:08:12 +0300

    Setup the environment and launch the page success

--- bitfinix/src/bitfinix/Task.java
+++ bitfinix/src/bitfinix/Task.java
@@ -1,20 +1,30 @@
 package bitfinix;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Task {
+	
+	// Declare global variable
+	public WebDriver driver; 
   
 	@BeforeTest
 	public void Setup()
 	{
+		// Local path to the chrome driver
+		System.setProperty("webdriver.chrome.driver", "/home/joe/Downloads/chromedriver");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		
 	}
 	@Test
   public void loadPage()
 	{
-		
+		// Load page
+		driver.get("https://www.bitfinex.com/");
   }
 	@AfterTest
 	public void stop()
