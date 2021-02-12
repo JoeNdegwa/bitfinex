eaf9dec09d0dca3e1490790e70b17052b271b98d
joe <joe@kali>
2021-02-12 22:17:43 +0300

    Accept cookies

--- bitfinix/src/bitfinix/Task.java
+++ bitfinix/src/bitfinix/Task.java
@@ -1,7 +1,11 @@
 package bitfinix;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -10,7 +14,8 @@ public class Task {
 	
 	// Declare global variable
 	public WebDriver driver; 
-  
+	WebDriverWait wait;
+	
 	@BeforeTest
 	public void Setup()
 	{
@@ -18,7 +23,7 @@ public class Task {
 		System.setProperty("webdriver.chrome.driver", "/home/joe/Downloads/chromedriver");
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		
+		wait = new WebDriverWait(driver, 20);
 	}
 	@Test
   public void loadPage()
@@ -26,6 +31,29 @@ public class Task {
 		// Load page
 		driver.get("https://www.bitfinex.com/");
   }
+	@Test
+	public void cookies()
+	{
+		// Need to wait for page to load
+		//Allow cookies
+		WebElement cookies = driver.findElement(By.xpath("//button[@class='bp3-button cookies_btn']"));
+		wait.until(ExpectedConditions.elementToBeClickable(cookies));
+		cookies.click();
+		//Agree to cookies popup
+		driver.switchTo().activeElement().click();
+		driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div/div[3]/div/button")).click();
+	}
+	@Test
+	public void search()
+	{
+		
+	}
+	@Test
+	public void clickResult()
+	{
+		
+	}
+	
 	@AfterTest
 	public void stop()
 	{
